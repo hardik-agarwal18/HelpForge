bdd15c4e6f7c6ca9061cf05fb18aa6dd9b78e8be
Hardik Agarwal <hardikagarwal9308@gmail.com>
2026-03-11 19:27:18 +0530

    feat(api-gateway): add dotenv dependency and set up basic Express server

--- services/api-gateway/src/app.js
+++ services/api-gateway/src/app.js
@@ -2,4 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from API Gateway!");
+});
+
 export default app;
