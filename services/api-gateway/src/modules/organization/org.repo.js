e2a0f39fbbed4657e9263ba76ac9c7d0aeb55c9b
Hardik Agarwal <hardikagarwal9308@gmail.com>
2026-03-12 22:10:26 +0530

    feat(organization): update createOrganization to include userId and memberships

--- services/api-gateway/src/modules/organization/org.repo.js
+++ services/api-gateway/src/modules/organization/org.repo.js
@@ -1,7 +1,36 @@
 import prisma from "../../config/database.config.js";
 
-export const createOrganization = async (name) => {
+export const createOrganization = async (name, userId) => {
   return await prisma.organization.create({
-    data: { name },
+    data: {
+      name,
+      memberships: {
+        create: {
+          userId,
+          role: "OWNER",
+        },
+      },
+    },
+    include: {
+      memberships: true,
+    },
+  });
+};
+
+export const getOrganizationById = async (id) => {
+  return await prisma.organization.findUnique({
+    where: { id },
+  });
+};
+
+export const getOrganizationsByUserId = async (userId) => {
+  return await prisma.organization.findMany({
+    where: {
+      memberships: {
+        some: {
+          userId,
+        },
+      },
+    },
   });
 };
