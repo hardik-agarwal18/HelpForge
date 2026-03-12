bae1f851b2efad1a40f17525e0fe7bd48095ee06
Hardik Agarwal <hardikagarwal9308@gmail.com>
2026-03-12 18:25:49 +0530

    feat(app): set up authentication routes and error handling middleware

--- services/api-gateway/src/app.js
+++ services/api-gateway/src/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import authRoutes from "./modules/auth/auth.routes.js";
+import { errorHandler } from "./utils/errorHandler.js";
 
 const app = express();
 
@@ -7,5 +9,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello from API Gateway!");
 });
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Error handler middleware (must be last)
+app.use(errorHandler);
 
 export default app;
